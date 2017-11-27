69d409496a85040b4e1881797192ba96d998ac69
York <yorkzero8312@gmail.com>
2017-11-27 22:38:10 +0900

    [ADD] POSE Detection

--- FaceTrackerPlus/DlibWrapper.mm
+++ FaceTrackerPlus/DlibWrapper.mm
@@ -14,6 +14,9 @@
 
 #include <dlib/opencv.h>
 
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
 //it will be an per-row buffer offset appear when debug mode
 #ifdef YORKDEBUG
 #define BUFFER_OFF_SET 8
@@ -28,9 +31,21 @@
 @end
 @implementation DlibWrapper {
     dlib::shape_predictor sp;
+    
+    //text on screen
+    std::ostringstream outtext;
+    
     std::vector<cv::Point3d> reprojectsrc;
     cv::Mat cam_matrix;
     cv::Mat dist_coeffs;
+    std::vector<cv::Point3d> object_pts;
+    std::vector<cv::Point2d> reprojectdst;
+    
+    //temp buf for decomposeProjectionMatrix()
+    cv::Mat out_intrinsics;
+    cv::Mat out_rotation;
+    cv::Mat out_translation;
+    
 }
 
 
@@ -57,15 +72,27 @@
 - (void)prepareOpenCv {
     
 #ifdef YORKDEBUG
-    colCount++;
-    if(colCount == width){
-        position += BUFFER_OFF_SET;
-        colCount = 0;
-    }
+    double K[9] = { 4.3486177595235137e+02, 0., 2.4043588951243723e+02, 0., 4.3489616629048959e+02, 1.7445540680734098e+02, 0., 0., 1. };
+    double D[5] = { -5.9966891967248651e-02, 1.4974351658287866e+00, 1.6301554829583304e-05, 2.0969593747627289e-03, -4.6666756285231967e+00 };
+#else
+    double K[9] = { 1.0207435696821417e+03, 0., 6.5282370756046294e+02, 0., 1.0211188482243954e+03, 3.6321597037804844e+02, 0., 0., 1. };
+    double D[5] = { -1.5340366244160525e-01, 4.3555857977835135e+00, 6.1251997031285140e-04, 1.1563522640927748e-03, -2.2481907177130477e+01 };
 #endif
     
-    double K[9] = { 6.5308391993466671e+002, 0.0, 3.1950000000000000e+002, 0.0, 6.5308391993466671e+002, 2.3950000000000000e+002, 0.0, 0.0, 1.0 };
-    double D[5] = { 7.0834633684407095e-002, 6.9140193737175351e-002, 0.0, 0.0, -1.3073460323689292e+000 };
+    object_pts.push_back(cv::Point3d(6.825897, 6.760612, 4.402142));     //#33 left brow left corner
+    object_pts.push_back(cv::Point3d(1.330353, 7.122144, 6.903745));     //#29 left brow right corner
+    object_pts.push_back(cv::Point3d(-1.330353, 7.122144, 6.903745));    //#34 right brow left corner
+    object_pts.push_back(cv::Point3d(-6.825897, 6.760612, 4.402142));    //#38 right brow right corner
+    object_pts.push_back(cv::Point3d(5.311432, 5.485328, 3.987654));     //#13 left eye left corner
+    object_pts.push_back(cv::Point3d(1.789930, 5.393625, 4.413414));     //#17 left eye right corner
+    object_pts.push_back(cv::Point3d(-1.789930, 5.393625, 4.413414));    //#25 right eye left corner
+    object_pts.push_back(cv::Point3d(-5.311432, 5.485328, 3.987654));    //#21 right eye right corner
+    object_pts.push_back(cv::Point3d(2.005628, 1.409845, 6.165652));     //#55 nose left corner
+    object_pts.push_back(cv::Point3d(-2.005628, 1.409845, 6.165652));    //#49 nose right corner
+    object_pts.push_back(cv::Point3d(2.774015, -2.080775, 5.048531));    //#43 mouth left corner
+    object_pts.push_back(cv::Point3d(-2.774015, -2.080775, 5.048531));   //#39 mouth right corner
+    object_pts.push_back(cv::Point3d(0.000000, -3.116408, 6.097667));    //#45 mouth central bottom corner
+    object_pts.push_back(cv::Point3d(0.000000, -7.415691, 4.070434));    //#6 chin corner
     
     //fill in cam intrinsics and distortion coefficients
     cam_matrix = cv::Mat(3, 3, CV_64FC1, K);
@@ -79,6 +106,14 @@
     reprojectsrc.push_back(cv::Point3d(-10.0, 10.0, -10.0));
     reprojectsrc.push_back(cv::Point3d(-10.0, -10.0, -10.0));
     reprojectsrc.push_back(cv::Point3d(-10.0, -10.0, 10.0));
+    
+    //reprojected 2D points
+    reprojectdst.resize(8);
+    
+    //temp buf for decomposeProjectionMatrix()
+    out_intrinsics = cv::Mat(3, 3, CV_64FC1);
+    out_rotation = cv::Mat(3, 3, CV_64FC1);
+    out_translation = cv::Mat(3, 1, CV_64FC1);
 }
 
 - (void)doWorkOnSampleBuffer2:(CMSampleBufferRef)sampleBuffer inRects:(NSArray<NSValue *> *)rects {
@@ -210,14 +245,88 @@
     size_t bufferWidth = CVPixelBufferGetWidth(pixelBuffer);
     size_t bufferHeight = CVPixelBufferGetHeight(pixelBuffer);
     size_t bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer);
-    unsigned char *pixel = (unsigned char *)CVPixelBufferGetBaseAddress(pixelBuffer);
-    cv::Mat image = cv::Mat(bufferHeight, bufferWidth, CV_8UC4, pixel, bytesPerRow);
+    //unsigned char *pixel = (unsigned char *)CVPixelBufferGetBaseAddress(pixelBuffer);
+    char *baseBuffer = (char *)CVPixelBufferGetBaseAddress(pixelBuffer);
+    cv::Mat image = cv::Mat(bufferHeight, bufferWidth, CV_8UC4, baseBuffer, bytesPerRow);
     
-    //dlib::cv_image<dlib::bgr_pixel> cimg(image);
+    dlib::cv_image<dlib::rgb_alpha_pixel> cimg(image);
     
     std::vector<dlib::rectangle> convertedRectangles = [DlibWrapper convertCGRectValueArray:rects];
     
-    cv::putText(image, "Test", cv::Point(50, 40), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
+    for (unsigned long j = 0; j < convertedRectangles.size(); ++j) {
+        dlib::rectangle oneFaceRect = convertedRectangles[j];
+        // detect all landmarks
+        dlib::full_object_detection shape = sp(cimg, oneFaceRect);
+        
+        //draw Point
+        for (unsigned int i = 0; i < 68; ++i)
+        {
+            circle(image, cv::Point(shape.part(i).x(), shape.part(i).y()), 2, cv::Scalar(0, 0, 255), -1);
+        }
+        
+        std::vector<cv::Point2d> image_pts;
+        //fill in 2D ref points, annotations follow https://ibug.doc.ic.ac.uk/resources/300-W/
+        image_pts.push_back(cv::Point2d(shape.part(17).x(), shape.part(17).y())); //#17 left brow left corner
+        image_pts.push_back(cv::Point2d(shape.part(21).x(), shape.part(21).y())); //#21 left brow right corner
+        image_pts.push_back(cv::Point2d(shape.part(22).x(), shape.part(22).y())); //#22 right brow left corner
+        image_pts.push_back(cv::Point2d(shape.part(26).x(), shape.part(26).y())); //#26 right brow right corner
+        image_pts.push_back(cv::Point2d(shape.part(36).x(), shape.part(36).y())); //#36 left eye left corner
+        image_pts.push_back(cv::Point2d(shape.part(39).x(), shape.part(39).y())); //#39 left eye right corner
+        image_pts.push_back(cv::Point2d(shape.part(42).x(), shape.part(42).y())); //#42 right eye left corner
+        image_pts.push_back(cv::Point2d(shape.part(45).x(), shape.part(45).y())); //#45 right eye right corner
+        image_pts.push_back(cv::Point2d(shape.part(31).x(), shape.part(31).y())); //#31 nose left corner
+        image_pts.push_back(cv::Point2d(shape.part(35).x(), shape.part(35).y())); //#35 nose right corner
+        image_pts.push_back(cv::Point2d(shape.part(48).x(), shape.part(48).y())); //#48 mouth left corner
+        image_pts.push_back(cv::Point2d(shape.part(54).x(), shape.part(54).y())); //#54 mouth right corner
+        image_pts.push_back(cv::Point2d(shape.part(57).x(), shape.part(57).y())); //#57 mouth central bottom corner
+        image_pts.push_back(cv::Point2d(shape.part(8).x(), shape.part(8).y()));   //#8 chin corner
+        
+        //result
+        cv::Mat rotation_vec;                           //3 x 1
+        cv::Mat rotation_mat;                           //3 x 3 R
+        cv::Mat translation_vec;                        //3 x 1 T
+        cv::Mat pose_mat = cv::Mat(3, 4, CV_64FC1);     //3 x 4 R | T
+        cv::Mat euler_angle = cv::Mat(3, 1, CV_64FC1);
+        
+        //calc pos
+        cv::solvePnP(object_pts, image_pts, cam_matrix, dist_coeffs, rotation_vec, translation_vec);
+        
+        //reproject
+        cv::projectPoints(reprojectsrc, rotation_vec, translation_vec, cam_matrix, dist_coeffs, reprojectdst);
+        
+        //draw axis
+        line(image, reprojectdst[0], reprojectdst[1], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[1], reprojectdst[2], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[2], reprojectdst[3], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[3], reprojectdst[0], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[4], reprojectdst[5], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[5], reprojectdst[6], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[6], reprojectdst[7], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[7], reprojectdst[4], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[0], reprojectdst[4], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[1], reprojectdst[5], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[2], reprojectdst[6], cv::Scalar(0, 0, 255));
+        line(image, reprojectdst[3], reprojectdst[7], cv::Scalar(0, 0, 255));
+        
+        //calc euler angle
+        cv::Rodrigues(rotation_vec, rotation_mat);
+        cv::hconcat(rotation_mat, translation_vec, pose_mat);
+        cv::decomposeProjectionMatrix(pose_mat, out_intrinsics, out_rotation, out_translation, cv::noArray(), cv::noArray(), cv::noArray(), euler_angle);
+        
+        //show angle result
+        outtext << "X: " << std::setprecision(3) << euler_angle.at<double>(0);
+        cv::putText(image, outtext.str(), cv::Point(50, 40), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
+        outtext.str("");
+        outtext << "Y: " << std::setprecision(3) << euler_angle.at<double>(1);
+        cv::putText(image, outtext.str(), cv::Point(50, 60), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
+        outtext.str("");
+        outtext << "Z: " << std::setprecision(3) << euler_angle.at<double>(2);
+        cv::putText(image, outtext.str(), cv::Point(50, 80), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
+        outtext.str("");
+
+    }
+    
+    //cv::putText(image, "Test", cv::Point(50, 40), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
     
     CVPixelBufferUnlockBaseAddress( pixelBuffer, 0 );
 }
